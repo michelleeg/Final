4707417cc5e1323634bb877a223fd8d24cd6eba1
Michelle E Gamble <mg79570@srcs.loc>
2017-12-15 11:30:38 -0800

    times table

---
+++ final_challege_functions.js
@@ -0,0 +1,24 @@
+function checkPrime(number) {
+    for (i = 2; i < number/2; i++) {
+        if ((number) % i == 0) {
+            return "not prime";
+        }
+    }
+    return "prime";
+}
+
+function timesTable(number) {
+
+    var rows = 10;
+    var cells = 10;
+
+    document.write("<table border=1>");
+    for (i = 1; i < rows; i++) {
+        document.write("<tr>");
+        for (j = 1; j < cells; j++) {
+            document.write("<td>", i * j, "</td>");
+        }
+        document.write("</tr>")
+    }
+    document.write("</table>")
+}
